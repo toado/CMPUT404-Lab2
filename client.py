bc06ce5c05a8bd4024895efc2123ce2fa971befb
Justin  Tan <jtan@Justins-MacBook-Pro.local>
2021-01-20 20:05:26 -0700

    Wrote client & echo_server from scratch

--- client.py
+++ client.py
@@ -1,78 +1,22 @@
-import socket, sys
+import socket
 
 HOST = "www.google.com"
-PORT = 80   # Default port with TCP for an HTTP server
-PAYLOAD = 'GET / HTTP/1.0\r\nHOST: {host}\r\n\r\n'
-BUFFER_SIZE = 4096
-
-# Creating a client socket (the endpoint of a conversation)
-def create_TCP_socket():
-    print("\nAttempting to create socket . . .")
-    try:
-        # AF_INET refers to IPV4
-        # SOCK_STREAM refers to TCP
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("Successfully created socket\n")
-        return sock
-
-    except OSError:
-        print("Failed to create socket: Error: {}\n".format(OSError))
-        sys.exit()
-
-# Get host IP
-def get_remote_ip():
-    print(f'Attempting to get IP for {HOST} . . .')
-    try:
-        remote_ip = socket.gethostbyname(HOST)
-
-    except socket.gaierror:
-        print("Hostname could be resolved. Exiting ...\n")
-        sys.exit()
-    
-    print("Successfully connected to {}\n".format(HOST))
-    return remote_ip
-
-# Send data to server
-def send_data(sock, payload):
-    print("Sending payload . . .")
-    try:
-        sock.sendall(payload.encode())
-
-    except socket.error:
-        print("Send failed\n")
-
-    print("Payload successfully sent!\n")
+PORT = 80
+PAYLOAD = f"GET / HTTP/1.1\r\nHost: {HOST}\r\n\r\n"
+BUFFERSIZE = 1024
 
 def main():
-    try:
-        # Try creating the socket and getting HOST IP then connecting
-        sock = create_TCP_socket()    
-        remote_ip = get_remote_ip()
-        sock.connect((remote_ip, PORT))
-        print("Socket connected to {} on IP: {}".format(HOST, remote_ip))
-
-        # Send data & shutdown
-        send_data(sock, PAYLOAD)
-        sock.shutdown(socket.SHUT_WR)
-
-        # Continue to accept all data until none left
-        full_data = b""
-        while True:
-            data = sock.recv(BUFFER_SIZE)
-            
-            if not data:
-                break
-            
-            full_data += data
-        
-        print(full_data)
-
-    except Exception as e:
-        print(e)
-
-    # Always close the socket at the end!
-    finally:
-        sock.close()
+    # create a TCP/IP socket to connect to the remote socket
+    # - then send data (byte obj) to that remote socket
+    # - print the data received (byte object)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        print("IP?? {}".format(socket.gethostbyname(HOST)))
+        sock.connect((socket.gethostbyname(HOST), PORT))
+        sock.sendall(PAYLOAD.encode(encoding='UTF-8'))
+        data = sock.recv(1024)
+    
+    print("Data received:\n{}".format(data))
 
 if __name__ == "__main__":
     main()
+    
---
+++ echo_server.py
@@ -0,0 +1,30 @@
+import socket
+
+HOST = ""   # Symbolic name meaning all available interfaces
+PORT = 8001 
+BUFFERSIZE = 1024
+
+def main():
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    # Set socket w/ option SO_REUSEADDR to reuse the local socket within a TIME_WAIT state (don't have to wait for timeout to expire)
+    # Bind (associate) the socket to its local address so clients can connect
+    # and listen to requests
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((HOST, PORT))
+    server.listen(1)
+    
+    # Continue to listen for requests and accept them until forcefully closed
+    while True:
+      conn, addr = server.accept()
+      print("Connection information: {}".format(conn))
+      print(f'Connected by: {addr}')
+
+      # Receive the incoming data and send it back to the client
+      with conn:
+        data = conn.recv(BUFFERSIZE)
+        print("Data received: {}\n".format(data))
+
+        conn.sendall(data)
+  
+if __name__ == "__main__":
+  main()
